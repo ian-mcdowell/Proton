6d92e06b2956779362b570d1b0158474b405db3c
wdmcdowell <wdmcdowell@iastate.edu>
2016-03-25 11:03:26 -0500

    Initial Commit

---
+++ Proton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proton
+//
+//  Created by McDowell, Ian J [ITACD] on 3/25/16.
+//  Copyright © 2016 Ian McDowell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
